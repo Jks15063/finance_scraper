99a76bc0eb7af11308be9b3f217bb1873fd41b7c
root <root@jake-Lenovo-G580.(none)>
2015-01-15 17:50:28 -0700

    removed nightmare, replaced with request + cheerio.  Runs 20 pages at a time now.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var util        = require('util')
 var debug       = require('debug')('app')
 var async       = require('async')
 var MongoClient = require('mongodb').MongoClient
-var Nightmare   = require('nightmare')
 var $           = require('cheerio')
+var request     = require('request')
 var xlsx        = require('node-xlsx')
 var moment      = require('moment')
 
@@ -33,59 +33,42 @@ Array.zip = function(left, right, combinerFunction) {
    return results
 }
 
-var stockSheet = xlsx.parse(__dirname + '/tickers.xlsx')
+//var stockSheet = xlsx.parse(__dirname + '/tickers.xlsx')
+var stockSheet = xlsx.parse(__dirname + '/Bank\ Tickers\ only.xlsx')
 var stockList = stockSheet[0].data.
    map(function(item) {
       return item[0]
    })
 
 var url = 'mongodb://localhost:27017/mydb'
+
 MongoClient.connect(url, function(err, db) {
    if(err) {
       debug(err)
    } else {
-      var collection = db.collection('finance');
+      var collection = db.collection('bank_data_2015');
       var missingStocks = []
-      async.eachSeries(stockList, function(stockName, callback) {
-         var me = this
-         if(stockName.trim().toLowerCase() !== 'ticker') {
+      async.eachLimit(stockList, 20, function(stockName, callback) {
+         if(stockName && stockName.trim().toLowerCase() !== 'ticker') {
             debug('Stock: ' + stockName)
             debug('http://finance.yahoo.com/q/ks?s=' + stockName + '+Key+Statistics')
-            this.nightmare = new Nightmare({timeout: 20000})
 
-            me.nightmare
-            .on('error', function(err) {
-               debug('ERROR:: ' + err)
-               missingStocks.push(stockName)
-               me.nightmare.teardownInstance()
-            })
-            .on('timeout', function(msg) {
-               debug('TIMEOUT: ' + msg)
-               missingStocks.push(stockName)
-               me.nightmare.teardownInstance()
-            })
-            .goto('http://finance.yahoo.com/q/ks?s=' + stockName + '+Key+Statistics')
-            .evaluate(function() {
-               var obj         = {}
-               var tempSummary = document.getElementById("yfi_rt_quote_summary")
-               obj.summary     = (tempSummary) ? tempSummary.outerHTML : null
-               var tempCenter  = document.getElementById("yfncsumtab")
-               obj.centerTable = (tempCenter) ? tempCenter.outerHTML : null
-               return obj
-            }, function(res) {
-               if(res && res.summary && res.centerTable) {
-                  var newEntry          = {}
-                  newEntry.date         = moment().format("YYYY-MM-DD")
-                  newEntry.stock        = stockName
-                  var parsedSummary     = $.load(res.summary)
-                  var parsedCenterTable = $.load(res.centerTable)
-                  var ticker            = parsedSummary('.time_rtq_ticker')
-                  newEntry.price        = ticker["0"].children[0].children[0].data
-                  var titlesObj         = parsedCenterTable('.yfnc_tablehead1')
-                  var valuesObj         = parsedCenterTable('.yfnc_tabledata1')
-                  var titles            = []
-                  var values            = []
-                  debug('Price: ' + ticker["0"].children[0].children[0].data)
+            request({
+               uri: 'http://finance.yahoo.com/q/ks?s=' + stockName + '+Key+Statistics'
+            }, function(error, response, body) {
+               if(error) {
+                  return callback(error)
+               } else {
+                  $              = $.load(body);
+                  var newEntry   = {}
+                  newEntry.date  = moment().format("YYYY-MM-DD")
+                  newEntry.stock = stockName
+                  newEntry.price = $('.time_rtq_ticker').text()
+                  var titlesObj  = $('.yfnc_tablehead1')
+                  var valuesObj  = $('.yfnc_tabledata1')
+                  var titles     = []
+                  var values     = []
+
                   titlesObj.each(function(i, elem) {
                      if(elem.children && elem.children[0].data) {
                         titles.push(elem.children[0].data)
@@ -104,31 +87,14 @@ MongoClient.connect(url, function(err, db) {
                      newEntry[title] = value
                      return
                   })
-                  // TODO fix upsert, it inserts even if record for that day exists
-                  //var match = {}
-                  //match.stock = stockName
-                  //match.date = moment().format("YYYY-MM-DD")
-                  //collection.update(match, newEntry, {upsert: true}, function(err, item) {
                   collection.insert(newEntry, function(err, item) {
                      if(err) {
                         debug(err)
+                        return callback(err)
+                     } else {
+                        return callback()
                      }
-                     me.nightmare.teardownInstance()
-                     return callback()
                   })
-               } else {
-                  debug('Failed to get HTML for ' + stockName)
-                  missingStocks.push(stockName)
-                  // missing a stock hangs it up for some reason so manually tear it down
-                  me.nightmare.teardownInstance()
-                  return callback()
-               }
-            })
-            .run(function(err, nightmare) {
-               if(err) {
-                  debug(err)
-               } else {
-                  debug('--------------')
                }
             })
          } else {
@@ -144,11 +110,4 @@ MongoClient.connect(url, function(err, db) {
          db.close()
       })
    }
-});
-
-
-
-
-
-
-
+})
